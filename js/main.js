faf27916aabc01ede8efe9c6d673db13bd8e3cd5
Zavizenov <g_zavizenov@tss-hr420-dm06.local>
2019-05-14 17:58:18 -0400

    Added js code and included svg code in html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 ( () => {
   //stu
   console.log('Fired!');
+
+  //select elements with JavaScript
+  let svgGraphic = document.getElementById("badgeSVG")
+
+  function logMyId() {
+    console.log(this.id);
+    this.style.opacity = 0.5
+  }
+
+  svgGraphic.addEventListener("click", logMyId);
+
+  let jpgGraphic = document.getElementById("badgeJPG")
+  jpgGraphic.addEventListener("click", logMyId);
+
 })();
