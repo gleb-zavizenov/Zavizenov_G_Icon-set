b50b70a47cf205d770f13004c7b713f390a2fe9d
Zavizenov <g_zavizenov@tss-hr420-dm06.fanshawec.ca>
2019-05-21 15:59:05 -0400

    Added several functions in JavaScript

--- js/main.js
+++ js/main.js
@@ -4,15 +4,21 @@
 
   //select elements with JavaScript
   let svgGraphic = document.getElementById("badgeSVG")
+  let mainHeadLine = document.querySelector(".main-headline")
+  let secondHeadline = document.querySelector("#second-line")
 
   function logMyId() {
+    mainHeadLine.textContent = "Now you are something else"
+    secondHeadline.textContent = "Wow! It is something else now!"
     console.log(this.id);
-    this.style.opacity = 0.5
+    this.style.opacity = 1
   }
 
   svgGraphic.addEventListener("click", logMyId);
 
   let jpgGraphic = document.getElementById("badgeJPG")
   jpgGraphic.addEventListener("click", logMyId);
+
+
 
 })();
